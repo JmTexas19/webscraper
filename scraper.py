e0dc19b5457b3c456bbc9d96f2d5254db8c8ca03
DazedFury <jmtexas19@gmail.com>
2020-11-21 03:01:30 -0600

    finish scrape

--- scraper.py
+++ scraper.py
@@ -13,9 +13,9 @@ scraper = cfscrape.create_scraper()
 
 #URL and textfile
 text_file = open("Output.html", "w", encoding='UTF-8')
-completeURL = 'https://boxnovel.com/novel/transcending-the-nine-heavens/chapter-989/'
+completeURL = 'https://www.wuxiaworld.com/novel/the-second-coming-of-gluttony/scog-chapter-1'
 
-#Array for storing URL's
+# Array for storing URL's
 URLArray = []
 
 while(completeURL != None):
@@ -29,26 +29,28 @@ while(completeURL != None):
     page_content = BeautifulSoup(page_response.content, "html.parser")
     page_content.prettify    
     
-    #Get Chapter
-    chapter = page_content.find('h2', {'class' : 'text-center'})
+    #Get CHAPTER
+    chapter = page_content.find('div', {'class' : 'caption'}).find('h4', {'class' : ''})
+    text_file.write(str(chapter))
+    print("Writing: " + str(chapter))
 
-    #In case header changes
-    if(chapter == None):
-        chapter = page_content.find('h3')
+    #--[Optional] In case header changes
+    #if(chapter == None):
+    #   chapter = page_content.find('h3')
+    #text_file.write('<h3>' + chapter.get_text() + '</h3>')
+    #print("Writing " + chapter.get_text())
+    #--
 
-    text_file.write('<h3>' + chapter.get_text() + '</h3>')
-    print("Writing " + chapter.get_text())
+    # Get ARTICLE
+    article = page_content.find('div', {'id' : 'chapter-content'})
     
-    #Get Article
-    article = page_content.find('div', {'class' : 'entry-content'})
+    # Scrape Text
+    text_file.write(str(article))
 
-    #Find and print all text with tag p
-    paragraphs = article.find_all('p')
-    for i in range(len(paragraphs)):
-        text_file.write(str(paragraphs[i]))
+    # Find link to next chapter
+    nextURL = page_content.find('li', {'class' : 'next'})
+    nextURLHTML = nextURL.find('a', {'class' : 'btn-link'})
 
-    #Find link to next chapter
-    nextURLHTML = page_content.find('a', {'class' : 'btn next_page'})
     if(nextURLHTML != None):
         nextURL = nextURLHTML.get('href')
     else:
@@ -69,7 +71,7 @@ while(completeURL != None):
         completeURL = nextURL
 
         #If nextURL is only half of what you need.
-        # completeURL = "https://novelplanet.com" + nextURL    
+        completeURL = "https://wuxiaworld.com" + nextURL    
     
     #FORMAT
     text_file.write("<p style=\"page-break-after: always;\">&nbsp;</p>")
