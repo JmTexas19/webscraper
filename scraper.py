5d7a5bef5a00c732738c1c7a76a007e0bb3534e3
DazedFury <jmtexas19@gmail.com>
2021-01-20 13:42:25 -0600

    first commit

--- scraper.py
+++ scraper.py
@@ -7,13 +7,14 @@ Created on Mon Nov  5 20:37:33 2018
 # Here, we're just importing both Beautiful Soup and the Requests library
 from bs4 import BeautifulSoup
 import cfscrape
+import time
 
 # returns a CloudflareScraper instance
 scraper = cfscrape.create_scraper()  
 
 #URL and textfile
 text_file = open("Output.html", "w", encoding='UTF-8')
-completeURL = 'https://novelonlinefull.com/chapter/jcq0358560421609692146/chapter_1'
+completeURL = 'https://www.lightnovelworld.com/novel/dual-cultivation-lightnovel-09011258/chapter-1'
 
 # Array for storing URL's
 URLArray = []
@@ -28,11 +29,34 @@ while(completeURL != None):
     #we use the html parser to parse the url content and store it in a variable.
     page_content = BeautifulSoup(page_response.content, "html.parser")
     page_content.prettify    
+
+    #If delay is necessary
+    while(page_content.find('h1', text='Too Many Requests') is not None):
+        time.sleep(5) 
+        # this is the url that we've already determined is safe and legal to scrape from.
+        page_link = completeURL
+        
+        # here, we fetch the content from the url, using the requests library
+        page_response = scraper.get(page_link)
+        
+        #we use the html parser to parse the url content and store it in a variable.
+        page_content = BeautifulSoup(page_response.content, "html.parser")
+        page_content.prettify   
+
+    ##################################################################################################
     
+    #Get ARTICLE
+    article = page_content.find('div', {'id' : 'vung_doc'})
+
     #Get CHAPTER
-    chapter = page_content.find('h1', {'class' : 'name_chapter'})
+    chapter = page_content.find('h2')
     text_file.write(str(chapter))
     print("Writing: " + str(chapter))
+
+    # GET LINK
+    nextURL = page_content.find('a', {'class' : 'next'})
+
+    ##################################################################################################
 
     #--[Optional] In case header changes
     #if(chapter == None):
@@ -40,15 +64,11 @@ while(completeURL != None):
     #text_file.write('<h3>' + chapter.get_text() + '</h3>')
     #print("Writing " + chapter.get_text())
     #--
-
-    # Get ARTICLE
-    article = page_content.find('div', {'id' : 'vung_doc'})
     
     # Scrape Text
     text_file.write(str(article))
 
-    # Find link to next chapter
-    nextURL = page_content.find('a', text='NEXT CHAPTER')
+    #Next Link
     if(nextURL != None):
         nextURL = nextURL.get('href')
     else:
@@ -69,7 +89,7 @@ while(completeURL != None):
         completeURL = nextURL
 
         #If nextURL is only half of what you need.
-        #completeURL = "https://www.royalroad.com" + nextURL    
+        completeURL = "https://www.lightnovelworld.com" + nextURL  
     
     #FORMAT
     text_file.write("<p style=\"page-break-after: always;\">&nbsp;</p>")
